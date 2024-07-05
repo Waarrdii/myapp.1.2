47cc02211afa5def9c12a83b73a3535581868e24
waarrdii <waarrdii@gmail.com>
2024-07-05 10:28:21 +0700

    table component

--- resources/js/Components/Table.vue
+++ resources/js/Components/Table.vue
@@ -1,12 +1,22 @@
 <template>
-    <ul>
-        <li v-for="customer in tableData" :key="customer.id">
-            {{ customer.nama }}
-        </li>
-        <li v-for="header in headerColumn" :key="header.id">
-            {{ header }}
-        </li>
-    </ul>
+    <div class="w-5/6 relative overflow-auto">
+        <table class="w-full text-nowrap table-fixed text-left uppercase rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th v-for="column in headerColumn" :key="column" scope="col" class="px-6 py-3">
+                        {{ column }}
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="data in tableData" :key="data.id" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td v-for="column in headerColumn" :key="column" scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{data[column]}}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script setup>
