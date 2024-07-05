af4963aeae98fbc19cc2719ab2bf6eaaae7ef75f
waarrdii <waarrdii@gmail.com>
2024-07-05 14:14:22 +0700

    bind data checkbox done

--- resources/js/Components/Table.vue
+++ resources/js/Components/Table.vue
@@ -1,16 +1,25 @@
 <template>
-    <div class="w-5/6 relative overflow-auto">
-        <table class="w-full text-nowrap table-fixed text-left uppercase rtl:text-right text-gray-500 dark:text-gray-400">
+    <div class="w-5/6 relative overflow-scroll">
+        <table class="w-full text-nowrap text-left uppercase rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                    <th v-for="column in headerColumn" :key="column" scope="col" class="px-6 py-3">
+                    <th v-for="column in headerColumn" 
+                    :key="column" 
+                    v-show="column !== 'id' && column !== 'created_at' && column !== 'updated_at'" 
+                    scope="col" class="px-6 py-3">
                         {{ column }}
                     </th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="data in tableData" :key="data.id" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <td v-for="column in headerColumn" :key="column" scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <tr v-for="data in tableData"
+                 :key="data.id" 
+                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <Checkbox name="selectData" :checked="checked" v-model="data.ischecked"/>
+                    <td v-for="column in headerColumn"
+                    :key="column"
+                    v-show="column !== 'id' && column !== 'created_at' && column !== 'updated_at'" 
+                    scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         {{data[column]}}
                     </td>
                 </tr>
@@ -21,7 +30,7 @@
 
 <script setup>
 // get data from index
-import { defineProps } from 'vue';
+import { defineProps,ref } from 'vue';
 
 const props = defineProps({
     tableData: Array,
@@ -29,5 +38,8 @@ const props = defineProps({
 });
 // end get data from index
 
+// checkbox component
+import Checkbox from './Checkbox.vue';
+const checked = ref(false)
 
 </script>
