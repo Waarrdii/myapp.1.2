86b106185f739371284995cf04e2277267633232
waarrdii <waarrdii@gmail.com>
2024-07-17 19:39:57 +0700

    design index

--- resources/js/Pages/Customer/Index.vue
+++ resources/js/Pages/Customer/Index.vue
@@ -1,10 +1,15 @@
 <template>
      <Head title="Dashboard" />
      <AuthenticatedLayout>
-        <NavLink class="bg-blue-500 text-white px-2 py-1 rounded-md" :href="route('customer.create')" >
-        <img :src="asset('images/add-user.svg')" alt="Add User">Add User
+        <div class="mx-5 my-5">
+         <NavLink class="bg-blue-500 sticky top-0 z-10 text-white px-2 py-1 rounded-md h-8 w-12" :href="route('customer.create')" >
+        <AddUser/>
         </NavLink>
-        <Table :tableData="tableData" :headerColumn="headerColumn" class="overflow-scroll relative w-5/6 h-[500px] mx-auto"></Table>
+        <div class="relative overflow-auto w-full h-[500px] rounded-md">
+         <Table :tableData="tableData" :headerColumn="headerColumn"></Table>
+        </div>
+        </div>
+        
      </AuthenticatedLayout>
 </template>
 
@@ -13,7 +18,7 @@ import Table from '@/Components/Table.vue';
 import { Head } from '@inertiajs/vue3';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import NavLink from '@/Components/NavLink.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
+import AddUser from '@/Components/Icons/AddUser.vue';
 
 // get data from route and sent to child component
 
