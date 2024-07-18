470d296adcc508865866958c1d5ff03fea643a65
waarrdii <waarrdii@gmail.com>
2024-07-18 12:04:39 +0700

    input form

--- resources/js/Pages/Customer/Create.vue
+++ resources/js/Pages/Customer/Create.vue
@@ -2,9 +2,13 @@
     <Head title="Add User" />
 
     <AuthenticatedLayout>
-        <div class="mx-auto dark:text-white">
-            add user
-        </div>
+        <form action="" class="max-w-md mx-auto mt-3 bg-white py-4 px-2">
+            <div class="relative z-0 w-full mb-5 group">
+                <input type="email" name="floating_email" id="floating_email" class="block py-2 px-0 w-full text-sm text-gray-900 bg-transprent border-0 border-b-2 border-gray-300 appearance-no dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer">
+            <label for="floating_email" class="peer-focus:font-medium absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform translate-y-6 -top-4 z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-75 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-1">Alamat email</label>
+            </div>
+            
+        </form>
     </AuthenticatedLayout>
 
 </template>
@@ -12,6 +16,8 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import TextInput from '@/Components/TextInput.vue';
 
 </script>
 
