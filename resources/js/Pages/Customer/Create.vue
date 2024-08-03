94f7f4625860990375538a3d81cbb37a71268424
waarrdii <waarrdii@gmail.com>
2024-08-03 13:55:25 +0700

    form done, backend crud start

--- resources/js/Pages/Customer/Create.vue
+++ resources/js/Pages/Customer/Create.vue
@@ -2,21 +2,21 @@
     <Head title="Add User" />
 
     <AuthenticatedLayout>
-        <form action="" class="max-w-md mx-auto mt-3 py-4 px-2 grid grid-cols-3 gap-3">
-            <InputForm type="text" name="customerId" id="customerId" labelText="Customer ID"/>
-            <InputForm type="text" name="nama" id="nama" labelText="Nama" class="col-span-2"/>
-            <InputForm type="text" name="Alamat" id="Alamat" labelText="Alamat" class="col-span-3"/>
-            <InputForm type="text" name="Kota" id="Kota" labelText="Kota" />
-            <InputForm type="text" name="Negara" id="Negara" labelText="Negara"/>
+        <form action="" class="max-w-md mx-auto mt-3 py-4 px-2 grid grid-cols-6 gap-3">
+            <InputForm type="text" name="customerId" id="customerId" labelText="Customer ID" class="col-span-2"/>
+            <InputForm type="text" name="nama" id="nama" labelText="Nama" class="col-span-4"/>
+            <InputForm type="text" name="Alamat" id="Alamat" labelText="Alamat" class="col-span-6"/>
+            <InputForm type="text" name="Kota" id="Kota" labelText="Kota" class="col-span-3"/>
+            <InputForm type="text" name="Negara" id="Negara" labelText="Negara" class="col-span-2"/>
             <InputForm type="text" name="KodePos" id="KodePos" labelText="Kode Pos"/>
-            <InputForm type="text" name="Kontak" id="Kontak" labelText="Kontak" />
-            <InputForm type="text" name="Telephone" id="Telephone" labelText="Telephone"/>
-            <InputForm type="email" name="email" id="email" labelText="Email"  />
-            <InputForm type="text" name="Type" id="Type" labelText="Type"/>
-            <InputForm type="text" name="Jenjang" id="Jenjang" labelText="Jenjang"/>
-            <InputForm type="text" name="Tingkatan" id="Tingkatan" labelText="Tingkatan"/>
-            <InputForm type="text" name="Kelas" id="Kelas" labelText="Kelas"/>
-            <InputForm type="number" name="Saldo" id="Saldo" labelText="Saldo"/>
+            <InputForm type="text" name="Kontak" id="Kontak" labelText="Kontak" class="col-span-3"/>
+            <InputForm type="text" name="Telephone" id="Telephone" labelText="Telephone" class="col-span-3"/>
+            <InputForm type="email" name="email" id="email" labelText="Email"  class="col-span-3"/>
+            <InputForm type="text" name="Type" id="Type" labelText="Type" class="col-span-3"/>
+            <InputForm type="text" name="Jenjang" id="Jenjang" labelText="Jenjang" class="col-span-2"/>
+            <InputForm type="text" name="Tingkatan" id="Tingkatan" labelText="Tingkatan" class="col-span-2"/>
+            <InputForm type="text" name="Kelas" id="Kelas" labelText="Kelas" class="col-span-2"/>
+            <InputForm type="number" name="Saldo" id="Saldo" labelText="Saldo" class="col-span-3"/>
            
         </form>
     </AuthenticatedLayout>
